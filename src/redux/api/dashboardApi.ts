b9154e2e9db004492f654605106317d3888df396
ballursajeed <ballursajeed@gmail.com>
2024-05-20 14:05:19 +0530

    updated dashboard api

--- src/redux/api/dashboardApi.ts
+++ src/redux/api/dashboardApi.ts
@@ -1,5 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { BarResponse, LineResponse, PieResponse, StatsResponse } from "../../types/api-types";
+import {
+  BarResponse,
+  LineResponse,
+  PieResponse,
+  StatsResponse,
+} from "../../types/api-types";
 
 export const dashboardApi = createApi({
   reducerPath: "dashboardApi",
@@ -7,24 +12,27 @@ export const dashboardApi = createApi({
     baseUrl: `${import.meta.env.VITE_SERVER}/api/v1/dashboard/`,
   }),
   endpoints: (builder) => ({
-
-    stats:builder.query<StatsResponse,string>({
-        query:id => `stats?id=${id}`,
+    stats: builder.query<StatsResponse, string>({
+      query: (id) => `stats?id=${id}`,
+      keepUnusedDataFor: 0,
     }),
 
-    pie:builder.query<PieResponse,string>({
-        query:id => `pie?id=${id}`,
+    pie: builder.query<PieResponse, string>({
+      query: (id) => `pie?id=${id}`,
+      keepUnusedDataFor: 0,
     }),
 
-    bar:builder.query<BarResponse,string>({
-        query:id => `bar?id=${id}`,
+    bar: builder.query<BarResponse, string>({
+      query: (id) => `bar?id=${id}`,
+      keepUnusedDataFor: 0,
     }),
 
-    line:builder.query<LineResponse,string>({
-        query:id => `line?id=${id}`,
+    line: builder.query<LineResponse, string>({
+      query: (id) => `line?id=${id}`,
+      keepUnusedDataFor: 0,
     }),
-
   }),
 });
 
-export const {useBarQuery,useStatsQuery,useLineQuery,usePieQuery} = dashboardApi;
+export const { useBarQuery, useStatsQuery, useLineQuery, usePieQuery } =
+  dashboardApi;
